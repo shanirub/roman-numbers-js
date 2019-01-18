bde662a49ec4ecfbb5559d8cdc1071494454b30f
Shani Rubovitch <shani.rubovitch@bitctrl.de>
2019-01-18 16:15:51 +0100

    push remote

--- roman.js
+++ roman.js
@@ -1,11 +1,22 @@
 // rewriting roman numbers in js
 
-let roman = "X"    
 
-alert(romanToDec(roman))
+let roman = "X"   
+
+let map = new Map()
+map.set(1, "I")
+map.set(5, "V")
+map.set(10, "X")
+map.set(50, "L")
+map.set(100, "C")
+map.set(500, "D")
+map.set(1000, "M")
+
+alert(roman + " = " + romanToDec(roman))
 
 function romanToDec(roman) {
     let dec
+
 
     return dec
 }
