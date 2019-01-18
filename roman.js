da07c6ce0bad4db0c9941f2cde71b83759cde0cf
Shani Rubovitch <shani.rubovitch@bitctrl.de>
2019-01-18 16:45:13 +0100

    map added

--- roman.js
+++ roman.js
@@ -4,19 +4,26 @@
 let roman = "X"   
 
 let map = new Map()
-map.set(1, "I")
-map.set(5, "V")
-map.set(10, "X")
-map.set(50, "L")
-map.set(100, "C")
-map.set(500, "D")
-map.set(1000, "M")
+map.set("I", 1)
+map.set("V", 5)
+map.set("X", 10)
+map.set("L", 50)
+map.set("C", 100)
+map.set("D", 500)
+map.set("M", 1000)
 
 alert(roman + " = " + romanToDec(roman))
 
 function romanToDec(roman) {
-    let dec
+    let currentDigit = map.get(roman.charAt(0))
 
+    if (roman.length == 1)
+        return currentDigit
 
-    return dec
+    let nextDigit = map.get(roman.charAt(1))
+
+    if (currentDigit < nextDigit)
+        currentDigit *= -1
+
+    return currentDigit + romanToDec(roman.substring(1))
 }
